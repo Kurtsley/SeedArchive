598ee809de8e49656734ebff3beb8902a0d27cac
Kurtsley <73447098+Kurtsley@users.noreply.github.com>
2021-12-01 21:50:30 -0600

    "Added edit buttons for more"

--- new_main.py
+++ new_main.py
@@ -4,11 +4,12 @@
 
 # imports
 from datetime import datetime
-from sqlite3.dbapi2 import Error
+from sqlite3.dbapi2 import Cursor, Error
 import tkinter as tk
 from tkinter import IntVar, StringVar, messagebox
 from pathlib import Path
 import sqlite3 as sql
+from tkinter.constants import END, HIDDEN
 
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path("./assets")
@@ -74,6 +75,70 @@ def update_quantity(value, barcode):
     sql = f"""
     UPDATE currentcrop SET "Quantity (g)" = {value}
     WHERE "Barcode ID" = '{barcode}'
+    """
+
+    cursor.execute(sql)
+
+    conn.commit()
+
+
+def update_location(value, barcode):
+    """ Updates the location in the database. """
+    conn = create_connection(relative_to_assets(currentdb))
+
+    cursor = conn.cursor()
+
+    sql = f"""
+    UPDATE currentcrop SET "Location" = {value}
+    WHERE "Barcode ID" = '{barcode}'
+    """
+
+    cursor.execute(sql)
+
+    conn.commit()
+
+
+def update_notes(value, barcode):
+    """ Updates the notes in the database. """
+    conn = create_connection(relative_to_assets(currentdb))
+
+    cursor = conn.cursor()
+
+    sql = f"""
+        UPDATE currentcrop SET "Notes" = {value}
+        WHERE "Barcode ID" = '{barcode}'
+    """
+
+    cursor.execute(sql)
+
+    conn.commit()
+
+
+def update_germ(value, barcode):
+    """ Updates the germ in the database. """
+    conn = create_connection(relative_to_assets(currentdb))
+
+    cursor = conn.cursor()
+
+    sql = f"""
+        UPDATE currentcrop SET "Germ %" = {value}
+        WHERE "Barcode ID" = '{barcode}'
+    """
+
+    cursor.execute(sql)
+
+    conn.commit()
+
+
+def update_tkw(value, barcode):
+    """ Updates the tkw in the database. """
+    conn = create_connection(relative_to_assets(currentdb))
+
+    cursor = conn.cursor()
+
+    sql = f"""
+        UPDATE currentcrop SET "TKW (g)" = {value}
+        WHERE "Barcode ID" = '{barcode}'
     """
 
     cursor.execute(sql)
@@ -433,7 +498,9 @@ class MainMenu(tk.Frame):
         self.lbl_notes = tk.Label(
             textvariable=self.text_notes,
             relief="raised",
-            font=(None, 18)
+            font=(None, 18),
+            wraplength=500,
+            justify='left'
         )
         self.lbl_notes.place(
             x=162,
@@ -453,6 +520,15 @@ class MainMenu(tk.Frame):
             width=164,
             height=40
         )
+
+        # Horizontal seperator
+        self.canvas.create_rectangle(
+            0.0,
+            656.0,
+            1024.0,
+            663.0,
+            fill="#868686",
+            outline="")
 
         # Scan button
         self.button_image_1 = tk.PhotoImage(
@@ -499,10 +575,10 @@ class MainMenu(tk.Frame):
             relief="flat"
         )
         self.but_add_quant.place(
-            x=479.0,
-            y=473.0,
-            width=101.0,
-            height=42.0
+            x=465.0,
+            y=476.0,
+            width=72.0,
+            height=40.0
         )
 
         # Remove quantity button
@@ -517,10 +593,10 @@ class MainMenu(tk.Frame):
         )
 
         self.but_rem_quant.place(
-            x=479,
-            y=531,
-            width=101,
-            height=42
+            x=465,
+            y=527,
+            width=72,
+            height=40
         )
 
         # Inventory button
@@ -540,21 +616,77 @@ class MainMenu(tk.Frame):
             height=52
         )
 
-        # Horizontal seperator
-        self.canvas.create_rectangle(
-            0.0,
-            656.0,
-            1024.0,
-            663.0,
-            fill="#868686",
-            outline="")
+        # Edit location button
+        self.button_image_6 = tk.PhotoImage(
+            file=relative_to_assets("edit.png"))
+        self.but_edit_location = tk.Button(
+            image=self.button_image_6,
+            border=0,
+            highlightthickness=0,
+            command=self.change_location,
+            relief='flat'
+        )
+        self.but_edit_location.place(
+            x=465,
+            y=341,
+            width=61,
+            height=40
+        )
+
+        # Edit tkw button
+        self.but_edit_tkw = tk.Button(
+            image=self.button_image_6,
+            border=0,
+            highlightthickness=0,
+            command=self.change_tkw,
+            relief='flat'
+        )
+        self.but_edit_tkw.place(
+            x=465,
+            y=584,
+            width=61,
+            height=40
+        )
+
+        # Edit germ button
+        self.but_edit_germ = tk.Button(
+            image=self.button_image_6,
+            border=0,
+            highlightthickness=0,
+            command=self.change_germ,
+            relief='flat'
+        )
+        self.but_edit_germ.place(
+            x=927,
+            y=584,
+            width=61,
+            height=40
+        )
+
+        # Edit notes button
+        self.but_edit_notes = tk.Button(
+            image=self.button_image_6,
+            border=0,
+            highlightthickness=0,
+            command=self.change_notes,
+            relief='flat'
+        )
+        self.but_edit_notes.place(
+            x=874,
+            y=902,
+            width=61,
+            height=40
+        )
 
     def date_convert(self):
         """ Convert date to correct format. """
         date = self.show_results(13)
         date = date[:10]
-        new_date = datetime.strptime(date, '%Y-%m-%d').strftime('%m/%d/%Y')
-        return new_date
+        if date == 'NA':
+            return "NA"
+        else:
+            new_date = datetime.strptime(date, '%Y-%m-%d').strftime('%m/%d/%Y')
+            return new_date
 
     def show_results(self, label_num):
         """ Shows the results in the labels. """
@@ -588,6 +720,40 @@ class MainMenu(tk.Frame):
         update_quantity(new, barcode)
         self.text_quantity.set(float(self.show_results(6)))
 
+    def change_location(self):
+        """ Change the location field. """
+        value = LocationChangePopup(self).show()
+        value = f'"{value}"'
+
+        barcode = self.text_barcode.get()
+        update_location(value, barcode)
+        self.text_location.set(self.show_results(9))
+
+    def change_notes(self):
+        """ Change the notes field. """
+        value = NotesChangePopup(self).show()
+        value = f'"{value}"'
+
+        barcode = self.text_barcode.get()
+        update_notes(value, barcode)
+        self.text_notes.set(self.show_results(12))
+
+    def change_germ(self):
+        """ Change the germ field. """
+        value = GermTKWChangePopup(self).show()
+
+        barcode = self.text_barcode.get()
+        update_germ(value, barcode)
+        self.text_germ.set(float(self.show_results(7)))
+
+    def change_tkw(self):
+        """ Change the tkw field. """
+        value = GermTKWChangePopup(self).show()
+
+        barcode = self.text_barcode.get()
+        update_tkw(value, barcode)
+        self.text_tkw.set(float(self.show_results(8)))
+
 
 class QuantityPopupAdd(object):
     """ Quantity edit window. """
@@ -617,7 +783,7 @@ class QuantityPopupAdd(object):
             row=1, columnspan=5, pady=5)
 
     def show(self):
-        """ Show the quantity window and return the grams to remove. """
+        """ Show the quantity window and return the grams to add or remove. """
         sw = self.master.winfo_screenwidth()
         sh = self.master.winfo_screenheight()
         w = 250
@@ -674,6 +840,139 @@ class QuantityPopupRemove(object):
         self.master.deiconify()
         self.master.wait_window()
         value = self.text_quantity_remove.get()
+        return float(value)
+
+
+class LocationChangePopup(object):
+    """ Change location popup. """
+
+    def __init__(self, master):
+        self.master = tk.Toplevel(master)
+        self.master.title("Edit Quantity")
+        self.master.grab_set()
+        self.master.focus_force()
+        self.master.resizable(False, False)
+
+        self.location_change = tk.StringVar()
+
+        frm1 = tk.Frame(self.master, padx=5, pady=5)
+        frm1.grid(row=0, column=1)
+
+        lbl = tk.Label(frm1, text="Enter new value:", pady=5,
+                       padx=5).pack()
+
+        frm2 = tk.Frame(self.master, padx=5, pady=5)
+        frm2.grid(row=0, column=2)
+
+        entry = tk.Entry(frm2, justify='left',
+                         width=20, textvariable=self.location_change).pack(pady=10, padx=5)
+
+        btn = tk.Button(self.master, text="Accept", padx=10, command=self.master.destroy).grid(
+            row=1, columnspan=5, pady=5)
+
+    def show(self):
+        """ Show the quantity window and return the grams to add or remove. """
+        sw = self.master.winfo_screenwidth()
+        sh = self.master.winfo_screenheight()
+        w = 250
+        h = 100
+        x = (sw / 2) - (w / 2)
+        y = (sh / 2) - (h / 2)
+        self.master.geometry("%dx%d+%d+%d" % (w, h, x, y))
+        self.master.attributes('-topmost', True)
+
+        self.master.deiconify()
+        self.master.wait_window()
+        value = self.location_change.get()
+        return str(value)
+
+
+class NotesChangePopup(object):
+    """ Change notes popup. """
+
+    def __init__(self, master):
+        self.master = tk.Toplevel(master)
+        self.master.title("Edit Quantity")
+        self.master.grab_set()
+        self.master.focus_force()
+        self.master.resizable(False, False)
+
+        self.text_notes = tk.StringVar()
+
+        frm1 = tk.Frame(self.master, padx=5, pady=5)
+        frm1.grid(row=0, column=1)
+
+        lbl = tk.Label(frm1, text="Enter new value:", pady=5,
+                       padx=5).pack()
+
+        frm2 = tk.Frame(self.master, padx=5, pady=5)
+        frm2.grid(row=0, column=2)
+
+        entry = tk.Entry(
+            frm2, width=50, textvariable=self.text_notes, font=(None, 10))
+        entry.pack(pady=10, padx=5)
+
+        btn = tk.Button(self.master, text="Accept", padx=10, command=self.master.destroy).grid(
+            row=1, columnspan=5, pady=5)
+
+    def show(self):
+        """ Show the quantity window and return the grams to add or remove. """
+        sw = self.master.winfo_screenwidth()
+        sh = self.master.winfo_screenheight()
+        w = 500
+        h = 90
+        x = (sw / 2) - (w / 2)
+        y = (sh / 2) - (h / 2)
+        self.master.geometry("%dx%d+%d+%d" % (w, h, x, y))
+        self.master.attributes('-topmost', True)
+
+        self.master.deiconify()
+        self.master.wait_window()
+        value = self.text_notes.get()
+        return value
+
+
+class GermTKWChangePopup(object):
+    """ Germ edit window. """
+
+    def __init__(self, master):
+        self.master = tk.Toplevel(master)
+        self.master.title("Edit Quantity")
+        self.master.grab_set()
+        self.master.focus_force()
+        self.master.resizable(False, False)
+
+        self.text_germ_change = tk.StringVar()
+
+        frm1 = tk.Frame(self.master, padx=5, pady=5)
+        frm1.grid(row=0, column=1)
+
+        lbl = tk.Label(frm1, text="Enter new value:", pady=5,
+                       padx=5).pack()
+
+        frm2 = tk.Frame(self.master, padx=5, pady=5)
+        frm2.grid(row=0, column=2)
+
+        entry = tk.Entry(frm2, justify='center',
+                         width=5, textvariable=self.text_germ_change).pack(pady=10, padx=5)
+
+        btn = tk.Button(self.master, text="Accept", padx=10, command=self.master.destroy).grid(
+            row=1, columnspan=5, pady=5)
+
+    def show(self):
+        """ Show the quantity window and return the grams to remove. """
+        sw = self.master.winfo_screenwidth()
+        sh = self.master.winfo_screenheight()
+        w = 250
+        h = 100
+        x = (sw / 2) - (w / 2)
+        y = (sh / 2) - (h / 2)
+        self.master.geometry("%dx%d+%d+%d" % (w, h, x, y))
+        self.master.attributes('-topmost', True)
+
+        self.master.deiconify()
+        self.master.wait_window()
+        value = self.text_germ_change.get()
         return float(value)
 
 
